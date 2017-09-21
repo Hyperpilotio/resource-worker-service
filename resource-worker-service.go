a91b7d131ad427f54d173e928a32f01c8107220c
Timothy Chen <tnachen@gmail.com>
2017-09-21 13:53:53 +0800

    WIP

--- resource-worker-service.go
+++ resource-worker-service.go
@@ -2,27 +2,42 @@ package main
 
 import (
 	"time"
-	"github.com/prometheus/client_golang/prometheus/promhttp"
+
 	"github.com/gin-gonic/gin"
+	"github.com/golang/glog"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
-type CPURequestParams struct {
-	Intensity float64 `form:"intensity" json:"intensity"`
-	Cycles    int     `form:"cycles" json:"cycles"`
+type ResourceWork interface {
+	Run() error
 }
 
-type MemoryRequestParams struct {
-	Intensity float64 `form:"intensity" json:"intensity"`
-	NumChunks int     `form:"numChunks" json:"numChunks"`
+type CPURequest struct {
+	Cycles int `form:"cycles" json:"cycles"`
+}
+
+func (request *CPURequest) Run() error {
+	for i := 0; i < request.Cycles; i += 1 {
+	}
+
+	return nil
+}
+
+type NetworkRequest struct {
+	Bandwidth int `form:"bandwidth" json:"bandwidth"`
+}
+
+type BlkIoRequest struct {
 }
 
 type ResourceRequest struct {
-	*CPURequestParams    `form:"cpu" json:"cpu,omitempty"`
-	*MemoryRequestParams `form:"memory" json:"memory,omitempty"`
+	*CPURequest     `form:"cpu" json:"cpu,omitempty"`
+	*NetworkRequest `form:"network" json:"network,omitempty"`
+	*BlkIoRequest   `from:"blkio" json:"blkio,omitempty"`
 }
 
-type Request struct {
-	Definitions []ResourceRequest `form:"definitions" json:"definitions"`
+type Work struct {
+	Requests []ResourceRequest `form:"definitions" json:"definitions"`
 }
 
 func main() {
@@ -31,15 +46,23 @@ func main() {
 	r.GET("/metrics", gin.WrapH(promhttp.Handler()))
 
 	r.POST("/run", func(c *gin.Context) {
-		var request Request
-		if c.BindJSON(&request) == nil {
+		var work Work
+		if err := c.BindJSON(&request); err == nil {
 			startTime := time.Now()
+			for _, definition := range request.Definitions {
+				if err := definition.Run(); err != nil {
+					// Error
+				}
+			}
+
 			// Run the worker
-			c.JSON(200, gin.H{
-				"message": "done",
-				"duration": time.Now().Sub(startTime).Seconds(),
+			c.JSON(http.StatusOk, gin.H{
+				"message":  "done",
+				"duration": time.Now().Sub(startTime),
 				// Put the results / summaries of the work in response
 			})
+		} else {
+			// Handle error
 		}
 	})
 
