a06bcb7911b732b8d13d9cc06410afac7df8579e
Timothy Chen <tnachen@gmail.com>
2017-10-11 12:28:46 -0700

    Add GCP deploy json

--- deploy-gcp.sh
+++ deploy-gcp.sh
@@ -2,12 +2,12 @@
 
 if [ "$#" -ne 1 ]
 then
-    echo "Usage: deploy-k8s.sh <userId>"
+    echo "Usage: deploy-gcp.sh <userId>"
     exit 1
 fi
 
 DEPLOYER_URL="localhost"
 
-curl -XPOST $DEPLOYER_URL:7777/v1/users/$1/deployments --data-binary @empty-k8s.json
+curl -XPOST $DEPLOYER_URL:7777/v1/users/$1/deployments --data-binary @deploy-gcp.json
 
 echo "Please check progress of your deployment at http://$DEPLOYER_URL:7777/ui"
